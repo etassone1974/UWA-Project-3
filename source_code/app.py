032d45dbc00658304d9103f4c9e56130af1a2284
Ezra Tassone <etassone@gmail.com>
2021-02-19 13:19:06 +0800

    Machine learning models

--- source_code/app.py
+++ source_code/app.py
@@ -1,109 +1,118 @@
-from flask import Flask, render_template, redirect, jsonify 
-from flask_pymongo import PyMongo
-from bson.json_util import dumps, loads
-import json
+from flask import Flask
+import numpy as np
+from flask_restful import Api
+from predict import Predict
+import requests
+from example import run_request
+from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+import pandas as pd
 
-import population_ETL
-
-# Flask Setup
+# Create APP
 app = Flask(__name__)
+API = Api(app)
 
-# Use flask_pymongo to set up mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/populationDB")
+# Load database
+# From prepared CSV files
+# Separate files for machine learning and other data due to data source limitations
+cyclone_data = pd.read_csv("Cyclone_1990_clean.csv")
+cyclone_ml_data = pd.read_csv("Cyclone_ML.csv")
 
+# from flask_sqlalchemy import SQLAlchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite" #os.environ.get('DATABASE_URL', '') or 
+
+# Remove tracking modifications
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+# Pet = create_classes(db)
 
 # Flask Routes
 
-# json API pages
-@app.route('/api/population/latest')
-def latest():
-    latest = mongo.db.latestPopulation.find() # PYMONGO CURSOR 
-    dump_latest = dumps(latest) # CONVERT PYMONGO CUSTOR TO JSON STRING 
-    json_latest = json.loads(dump_latest) # CONVERT TO ACTUAL JSON 
-    return jsonify(json_latest)  # RETURN JSON 
+# Add predict to route predict
+API.add_resource(Predict, '/predict')
 
-@app.route("/api/population/countries")
-def countries():
-    countries = mongo.db.countriesPopulation.find() # PYMONGO CURSOR 
-    dump_countries = dumps(countries) # CONVERT PYMONGO CUSTOR TO JSON STRING 
-    json_countries = json.loads(dump_countries) # CONVERT TO ACTUAL JSON 
-    return jsonify(json_countries)   # RETURN JSON 
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route('/api/population/cities')
-def cities():
-    cities = mongo.db.citiesPopulation.find()
-    dump_cities = dumps(cities)
-    json_cities = json.loads(dump_cities)
-    return jsonify(json_cities) 
+# @app.route("/viz1")
+# def viz1():
+#     return render_template('viz1.html')
 
-@app.route('/api/population/world')
-def world():
-    data = mongo.db.worldPopulation.find()
-    dump_data = dumps(data)
-    json_data = json.loads(dump_data)
-    return jsonify(json_data) 
+@app.route('/example')
+def run_example():
+    res = run_request()
+    return res
 
-@app.route('/api/population/density')
-def density():
-    data = mongo.db.populationDensity.find()
-    dump_data = dumps(data)
-    json_data = json.loads(dump_data)
-    return jsonify(json_data) 
+@app.route('/parameters/<surface_code>&<cyc_type>&<lat>&<lon>&<central_pres>&<max_wind_spd>')
+def get_prediction(surface_code=1, cyc_type=20, lat=-11, lon=92.6, central_pres=1001, max_wind_spd=12.9):
+    url = 'http://127.0.0.1:5000/predict'
+    body = {
+        "surface_code": surface_code,
+        "cyc_type": cyc_type,
+        "lat": lat,
+        "lon": lon,
+        "central_pres" : central_pres,
+        "max_wind_spd" : max_wind_spd
+    }
+    response = requests.post(url, data=body)
+    return response.json()
 
-@app.route('/api/population/birth')
-def birth():
-    data = mongo.db.birthRate.find()
-    dump_data = dumps(data)
-    json_data = json.loads(dump_data)
-    return jsonify(json_data) 
-
-@app.route('/api/population/death')
-def death():
-    data = mongo.db.deathRate.find()
-    dump_data = dumps(data)
-    json_data = json.loads(dump_data)
-    return jsonify(json_data) 
-
-@app.route('/api/population/sex-ratio')
-def sexRatio():
-    data = mongo.db.sexRatio.find()
-    dump_data = dumps(data)
-    json_data = json.loads(dump_data)
-    return jsonify(json_data) 
+# @app.route('/create/<first_name>&<last_name>')
+# def create(first_name=None, last_name=None):
+#   return 'Hello ' + first_name + ',' + last_name
 
 
-# html pages
-@app.route('/')
-def index():
-    return render_template('index.html')
+# # Query the database and send the jsonified results
+# @app.route("/send", methods=["GET", "POST"])
+# def send():
+#     if request.method == "POST":
+#         name = request.form["petName"]
+#         lat = request.form["petLat"]
+#         lon = request.form["petLon"]
 
-@app.route('/countries-population')
-def countriesPopulation():
-    return render_template('countries-population.html') 
+#         pet = Pet(name=name, lat=lat, lon=lon)
+#         db.session.add(pet)
+#         db.session.commit()
+#         return redirect("/", code=302)
 
-@app.route('/summary')
-def summary():
-    return render_template('summary.html')    
+#     return render_template("form.html")
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')  
 
-@app.route('/Data')
-def Data():
-    return render_template('Data.html')  
+# @app.route("/api/pals")
+# def pals():
+#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
 
-@app.route('/data-cities')
-def dataCities():
-    return render_template('data-cities.html') 
+#     hover_text = [result[0] for result in results]
+#     lat = [result[1] for result in results]
+#     lon = [result[2] for result in results]
 
-@app.route('/api')
-def api():
-    return render_template('api.html') 
+#     pet_data = [{
+#         "type": "scattergeo",
+#         "locationmode": "USA-states",
+#         "lat": lat,
+#         "lon": lon,
+#         "text": hover_text,
+#         "hoverinfo": "text",
+#         "marker": {
+#             "size": 50,
+#             "line": {
+#                 "color": "rgb(8,8,8)",
+#                 "width": 1
+#             },
+#         }
+#     }]
 
-@app.route('/header')
-def header():
-    return render_template('header.html') 
+#     return jsonify(pet_data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port='80')
