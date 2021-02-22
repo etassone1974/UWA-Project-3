6fcc45afbc3dd89903205c39596f730a2c3613ab
Ezra Tassone <etassone@gmail.com>
2021-02-22 23:06:07 +0800

    Updated example.py

--- example.py
+++ example.py
@@ -8,11 +8,8 @@ def run_request():
         "lat": -12.54,
         "lon": 130.98,
         "central_pres" : 994,
-        "max_wind_spd" : 20.6
+        "max_wind_spd" : 20.6,
+        "central_index" : 2.721183568
     }
-
-    # Calculate central_index and add to dictionary
-    body["central_index"] = pow((0.186*(pow(3.45*(1010 - (body["central_pres"])),0.644))),0.746)
-    
     response = requests.post(url, data=body)
     return response.json()
